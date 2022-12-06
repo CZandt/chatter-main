58e481a19157950820e31cadda1f3882009a269c
CZandt <cole0hardy@gmail.com>
2022-12-06 16:18:50 -0700

    deployment settings

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://web-production-b8ee.up.railway.app/']
 
 # Application definition
 
